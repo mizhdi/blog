792020913c8e84adc60cf46a9c889af81b32d66c
mizhengdi <mizhdi@gmail.com>
2015-01-19 15:23:26 +0800

    delete jquery

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,41 +1,20 @@
 window.onload = function() {
 
-  setTimeout(function() {
-    document.querySelector('#loading').style.display = 'none';
-  }, 2500)
+  // setTimeout(function() {
+  //   document.querySelector('#loading').style.display = 'none';
+  // }, 2500)
 
   var btn = document.querySelector('#off-canvas-btn');
-  btn.addEventListener('click', function() {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
     document.querySelector('body').classList.toggle('active');
   }, false);
 
   var user = document.querySelector('.user-info');
   var avatar = user.querySelector('.avatar');
-  avatar.addEventListener('click', function() {
+  avatar.addEventListener('click', function(e) {
+    e.preventDefault();
     user.classList.toggle('open');
   }, false);
 
 }
-
-
-  // $('#load-more>a').on('click', function() {
-
-  //   $.ajax({
-  //     url: '/api/posts.json',
-  //     success: function(data) {
-  //       console.log(data);
-  //       var count = 1, content = "", delta = 0, count_sup = count + 5;
-  //       $.each(data, function(i, item) {
-  //         console.log(item);
-  //         if (i >= count & i < count_sup) {
-  //           content += '<article><h2><a class="post-link" href="' + item.link + '">' + item.title + '</a></h2>' + 
-  //                      '<p>' + item.desc + '</p><footer>' + item.date + '</footer></article>';
-  //           delta++;
-  //         }
-  //       });
-  //       count += delta;
-  //       $('.article-list').append(content);
-  //     }
-  //   });
-
-  // });
